a901c50dd0cddb413c5576bb7a9a3114d5d3a59b
oleger2008 <otsoy1995@yandex.ru>
2023-10-18 02:20:20 +0300

    add main.go and some deps

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,34 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
 
-	"github.com/fatih/color"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	desc "github.com/oleger2008/authentication_service/pkg/user/v1"
 )
 
+const grpcPort = 50051
+
+type Server struct {
+	desc.UnimplementedUserV1Server
+}
+
 func main() {
-	fmt.Println(color.GreenString("Hello, world!"))
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	server := grpc.NewServer()
+	reflection.Register(server)
+	desc.RegisterUserV1Server(server, &Server{})
+
+	log.Printf("Server is listening at %v", listener.Addr())
+
+	if err = server.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 }
